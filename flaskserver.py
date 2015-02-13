951705ba96d16f5e7a29f73ef60d62a3a05d016f
fuweixiao <weixiaofu2014@u.northwestern.edu>
2015-02-13 13:05:27 -0500

    upload file done

--- flaskserver.py
+++ flaskserver.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def loadpage(name = 'YYY'):
+def loadpage(name = None):
     return render_template('index.html', name = name)
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
-    print "hehe"
     if request.method == 'GET':
         return render_template('index.html')
     if request.method == 'POST':
         f = request.files["file"]
-        print "test!"
-        f.save('/home/smile/workspace/cuda_inpainting/uploads/test.img')
-        return render_template('index.html')
+        f.save('/home/smile/workspace/cuda_inpainting/static/test.png')
+        print url_for('static', filename='test.png')
+        return render_template('index.html', name = 'uploaded')
 if __name__ == '__main__':
     app.debug = True;
     app.run('0.0.0.0')
